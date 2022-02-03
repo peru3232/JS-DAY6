2a806d920648283496ec1e1dab662df1747d21ee
Joshua Powell <yoshuapovell@gmail.com>
2022-02-03 10:57:07 +0100

    begin basic ex

---
+++ Joshua/basic/script.js
@@ -0,0 +1,23 @@
+var sand = JSON.parse(sandwiches);
+console.table(sand);
+
+var fri= JSON.parse(fries);
+console.table(fri);
+
+
+
+
+/*
+for (let i = 0; i < arr.length; i++) {
+    document.getElementById("result").innerHTML += `
+  <div class="card m-2" style="width: 18rem;">
+  <img src="${arr[i].img}" class="card-img-top" alt="${arr[i].name}">
+  <div class="card-body">
+    <h5 class="card-title">${arr[i].name}</h5>
+    <p class="card-text">I am working as ${arr[i].jobTitle}.</p>
+  </div>
+</div>
+  `;
+}
+
+*/
