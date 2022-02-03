53878b7352d9f4a64cdde15f3bc761c73e52fdfd
peru3232 <prw@qmail.com.de>
2022-02-03 14:42:05 +0100

    advanced special :-)

--- Peter/Advanced/script.js
+++ Peter/Advanced/script.js
@@ -6,23 +6,20 @@ const output = document.getElementById("output");
 
 //output as cards: 
 // I. fixed Table Head
-let stacked = `
-`
+let stacked ='' //actual no header
+
 // II. Data from the object:
-for (obj of arrBooks) {
+for (obj in arrBooks) {
     stacked += `
-    <div class="card my-3 p-3 books" style="width: 18rem;">
-        <img src="${obj.img}" class="card-img-top" height="350" alt="...">
+    <div class="card my-3 p-3 books" id="${obj}" style="width: 18rem;">
+        <img src="${arrBooks[obj].img}" class="card-img-top" height="350" alt="...">
         <div class="card-body px-2 pb-0">
-            <h5 class="card-title3">${obj.title}</h5>
-            <p class="card-text"><hr>from ${obj.author}</p>
+            <h5 class="card-title3">${arrBooks[obj].title}</h5>
+            <p class="card-text"><hr>from ${arrBooks[obj].author}</p>
         </div>
     </div>`
-  }
+}
 
-// III. End of tableLayout: 
-stacked += `
-    `
 
 // IV. Output on html
 // console.log(stacked);
@@ -31,10 +28,18 @@ output.innerHTML = stacked;
 //get access to all cards
 const arrCards = document.getElementsByClassName("books");
 
-function changeReadProperty() {
-    this.style.background = 'gray';
+//toggle on arraydata true <-> false and background  white <-> gray
+function toggleReadProperty() {
+    if (!arrBooks[this.id].read){
+        this.style.background = 'gray';
+        arrBooks[this.id].read = true;
+    } else {
+        this.style.background = 'white';
+        arrBooks[this.id].read = false;
+    }
 }
 
+//add event listener to all cards
 for (obj of arrCards) {
-    obj.addEventListener("click", changeReadProperty);
+    obj.addEventListener("click", toggleReadProperty);
 }
